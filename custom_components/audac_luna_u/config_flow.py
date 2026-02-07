ab9edc54f1a066ebe8f1c1708bad8d86785e8b5b
maeneak <20371477+maeneak@users.noreply.github.com>
2026-02-07 20:51:28 +1000

    Add device address to config flow

--- custom_components/audac_luna_u/config_flow.py
+++ custom_components/audac_luna_u/config_flow.py
@@ -37,6 +37,9 @@ STEP_USER_DATA_SCHEMA = vol.Schema(
         vol.Optional(CONF_PORT, default=DEFAULT_PORT): vol.All(
             vol.Coerce(int), vol.Range(min=1, max=65535)
         ),
+        vol.Optional(CONF_ADDRESS, default=DEFAULT_ADDRESS): vol.All(
+            vol.Coerce(int), vol.Range(min=1, max=255)
+        ),
     }
 )
 
